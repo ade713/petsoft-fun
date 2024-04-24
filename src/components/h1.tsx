00aa45c8f622a5e53ec0373d04bafcf53e537a87
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-04-24 14:21:16 -0700

    - build branding and stats components

---
+++ src/components/h1.tsx
@@ -0,0 +1,11 @@
+type H1Props = {
+  children: React.ReactNode;
+};
+
+export function H1({ children }: H1Props) {
+  return (
+    <h1 className="font-medium text-2xl leading-6">
+      {children}
+    </h1>
+  );
+}
