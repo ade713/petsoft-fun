66dfaffb5b15993a1d859a1a5a2cecb83263d5e4
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-04-28 21:25:43 -0700

    - create constants file, move default imageUrl to file

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -5,8 +5,8 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
-
-const DEFAULT_PET_IMAGE_URL = "https://images.unsplash.com/photo-1545218553-cdb549f13f8a?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+import { addPet } from "@/actions/actions";
+import { DEFAULT_PET_IMAGE_URL } from "@/lib/constants";
 
 type PetFormProps = {
   actionType: "add" | "edit";
---
+++ src/lib/constants.ts
@@ -0,0 +1,1 @@
+export const DEFAULT_PET_IMAGE_URL = "https://images.unsplash.com/photo-1545218553-cdb549f13f8a?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
