087f062e63bc38304bae5b95a02efec4c9d83942
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-04-28 20:25:47 -0700

    - get pet data from db instead of fetching from server

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -3,19 +3,14 @@ import { AppHeader } from "@/components/app-header";
 import { BackgroundPattern } from "@/components/background-pattern";
 import { PetContextProvider } from "@/contexts/pet-context-provider";
 import { SearchContextProvider } from "@/contexts/search-context-provider";
-import { Pet } from "@/lib/types";
+import prisma from "@/lib/db";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 export default async function Layout({ children }: LayoutProps) {
-  const response = await fetch("https://bytegrad.com/course-assets/projects/petsoft/api/pets");
-
-  if (!response.ok) {
-    throw new Error("Could not retrieve pet data");
-  }
-  const data: Pet[] = await response.json();
+  const petData = await prisma.pet.findMany();
 
   return (
     <>
@@ -24,7 +19,7 @@ export default async function Layout({ children }: LayoutProps) {
       <div className="flex flex-col max-w-[1050px] min-h-screen mx-auto px-4">
         <AppHeader />
         <SearchContextProvider>
-          <PetContextProvider petData={data}>
+          <PetContextProvider petData={petData}>
             {children}
           </PetContextProvider>
         </SearchContextProvider>
