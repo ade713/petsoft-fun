3fed8da0515e4bd177e32d3d2fb878ee3a42e86c
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-05-05 02:00:10 -0700

    - only set default values for pet form when editing a pet

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -25,13 +25,16 @@ export function PetForm({ actionType, onFormSubmitted }: PetFormProps) {
     formState: { errors },
   } = useForm<TPetForm>({
     resolver: zodResolver(petFormSchema),
-    defaultValues: {
-      name: selectedPet?.name,
-      ownerName: selectedPet?.ownerName,
-      imageUrl: selectedPet?.imageUrl,
-      age: selectedPet?.age,
-      notes: selectedPet?.notes,
-    },
+    defaultValues:
+      actionType === "edit"
+        ? {
+            name: selectedPet?.name,
+            ownerName: selectedPet?.ownerName,
+            imageUrl: selectedPet?.imageUrl,
+            age: selectedPet?.age,
+            notes: selectedPet?.notes,
+          }
+        : undefined,
   });
 
   async function handlePetFormAction() {
